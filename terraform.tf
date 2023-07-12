1aa32aff3e2b66c25c0d2a62820a3ee6848118a0
Zdenek Vydra <xvydra@fi.muni.cz>
2023-07-12 19:25:18 +0200

    Use terraform sandbox module, add pool_url

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "http" {
   }
-
   required_providers {
     kypo = {
       source  = "vydrazde/kypo"
@@ -10,64 +9,30 @@ terraform {
   }
 }
 
-provider "kypo" {
-  endpoint  = local.endpoint
-  client_id = "bzhwmbxgyxALbAdMjYOgpolQzkiQHGwWRXxm"
-}
-
 locals {
-  stages   = toset(["user-ansible", "networking-ansible", "terraform"])
   endpoint = "https://images.crp.kypo.muni.cz"
+  pool_url = "${local.endpoint}/pool/${module.sandbox.pool_id}"
 }
 
 variable "CI_PROJECT_URL" {}
 variable "CI_COMMIT_SHA" {}
 
-resource "kypo_sandbox_definition" "definition" {
-  url = replace(var.CI_PROJECT_URL, "/https://([^/]+)/(.+)/", "git@$1:$2.git")
-  rev = var.CI_COMMIT_SHA
+provider "kypo" {
+  endpoint  = local.endpoint
+  client_id = "bzhwmbxgyxALbAdMjYOgpolQzkiQHGwWRXxm"
 }
 
-resource "kypo_sandbox_pool" "pool" {
-  definition = {
-    id = kypo_sandbox_definition.definition.id
-  }
-  max_size = 2
+module "sandbox" {
+  source      = "vydrazde/sandbox/kypo"
+  project_url = var.CI_PROJECT_URL
+  rev         = var.CI_COMMIT_SHA
 }
 
-resource "kypo_sandbox_allocation_unit" "sandbox" {
-  pool_id                       = kypo_sandbox_pool.pool.id
-  warning_on_allocation_failure = true
-}
-
-data "kypo_sandbox_request_output" "output" {
-  id    = kypo_sandbox_allocation_unit.sandbox.allocation_request.id
-  stage = each.key
-
-  for_each = local.stages
-}
-
-resource "local_file" "output" {
-  content         = data.kypo_sandbox_request_output.output[each.key].result
-  filename        = "outputs/p${kypo_sandbox_pool.pool.id}-s${kypo_sandbox_allocation_unit.sandbox.id}-${each.key}.txt"
-  file_permission = "666"
-
-  for_each = local.stages
-}
-
-resource "null_resource" "check" {
-  triggers = {
-    stages = join(", ", kypo_sandbox_allocation_unit.sandbox.allocation_request.stages)
-  }
-
-  lifecycle {
-    postcondition {
-      condition     = join(", ", kypo_sandbox_allocation_unit.sandbox.allocation_request.stages) == "FINISHED, FINISHED, FINISHED"
-      error_message = "Allocation has finished with errors"
-    }
-  }
+resource "local_file" "pool_url" {
+  filename = "vars.env"
+  content  = "POOL_URL=${local.pool_url}"
 }
 
 output "pool_url" {
-  value = "${local.endpoint}/pool/${kypo_sandbox_pool.pool.id}"
+  value = local.pool_url
 }
