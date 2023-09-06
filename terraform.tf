d12cb76fafb9287e89bae73874d7bb77c5d2cd15
Zdeněk Vydra <485514@mail.muni.cz>
2023-09-06 11:12:04 +0200

    Move KYPO endpoint and client id to vatiables

--- terraform.tf
+++ terraform.tf
@@ -9,23 +9,19 @@ terraform {
   }
 }
 
-locals {
-  endpoint = "https://images.crp.kypo.muni.cz"
-}
-
+variable "KYPO_ENDPOINT" {}
 variable "CI_PROJECT_URL" {}
 variable "CI_COMMIT_SHORT_SHA" {}
 
 provider "kypo" {
-  endpoint  = local.endpoint
-  client_id = "bzhwmbxgyxALbAdMjYOgpolQzkiQHGwWRXxm"
+  endpoint  = var.KYPO_ENDPOINT
 }
 
 module "sandbox" {
   source        = "gitlab.ics.muni.cz/muni-kypo-images/sandbox-ci/kypo"
   project_url   = var.CI_PROJECT_URL
   rev           = var.CI_COMMIT_SHORT_SHA
-  kypo_endpoint = local.endpoint
+  kypo_endpoint = var.KYPO_ENDPOINT
 }
 
 output "pool_url" {
